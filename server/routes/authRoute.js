4b89de82d16fcd98f17ba2ee3ab30b5d2b397e8e
AdityaGaur7 <adgaur027@gmail.com>
2024-12-25 02:42:10 +0530

    protecting routes using requiresignin and isadmin

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { registerController, loginController } from '../controllers/authController.js';
-
+import {requireSignIn,isAdmin} from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 router.post('/register', registerController);
 router.post('/login', loginController);
+router.get('/profile', requireSignIn,isAdmin, (req, res) => {
+    res.send('ok');
+});
 
 
 export default router;
